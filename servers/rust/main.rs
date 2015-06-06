580997ec8f2b37e78cac1e6e00cffea18ed26012
Jon Gjengset <jon@thesquareplanet.com>
2015-06-06 14:05:28 -0400

    Add rust server

---
+++ servers/rust/main.rs
@@ -0,0 +1,67 @@
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::io;
+use std::env;
+use std::mem;
+use std::str::FromStr;
+use std::io::Read;
+use std::io::Write;
+
+fn main() {
+    let port_ : Option<String> = env::args().skip(2).next();
+    if let None = port_ {
+        println!("usage: {} -p port", env::args().next().unwrap());
+    }
+
+    let port = u16::from_str(&port_.unwrap());
+    if let Err(ref e) = port {
+        println!("invalid port number given: {}", e);
+    }
+    let listener = TcpListener::bind(("127.0.0.1", port.unwrap())).unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move|| {
+                    handle_client(stream)
+                });
+            }
+            Err(e) => {
+                println!("failed to accept connection: {}", e);
+            }
+        }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buf = [0_u8; 4];
+    let mut nread;
+    loop {
+        nread = 0;
+        while nread < buf.len() {
+            match stream.read(&mut buf[nread..]) {
+                Ok(n) if n == 0 => return,
+                Ok(n) => nread += n,
+                Err(ref e) if e.kind() == io::ErrorKind::Interrupted => {}
+                Err(e) => {
+                    println!("{}", e);
+                    return;
+                }
+            }
+        }
+        let challenge : &mut u32 = unsafe{ mem::transmute(&mut buf) };
+        *challenge += 1;
+
+        let mut nwritten = 0;
+        while nwritten < buf.len() {
+            match stream.write(&buf[nwritten..]) {
+                Ok(n) => nwritten += n,
+                Err(ref e) if e.kind() == io::ErrorKind::Interrupted => {}
+                Err(e) => {
+                    println!("{}", e);
+                    return;
+                }
+            }
+        }
+    }
+}
