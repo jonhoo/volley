d9c6dec39f3489bb8acd9e76848c1755d81e1491
Jon Gjengset <jon@thesquareplanet.com>
2015-06-06 21:54:16 -0400

    Make c server threaded

---
+++ servers/cthread/main.c
@@ -0,0 +1,136 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/sysinfo.h>
+#include <signal.h>
+#include <pthread.h>
+
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#include <sys/wait.h>
+
+static int done = 0;
+
+void * handle_client(void * arg);
+void sig_handler(int signo) {
+	if (signo == SIGINT || signo == SIGKILL || signo == SIGTERM) {
+		done = 1;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int opt, ret;
+	int port;
+	long i;
+
+	struct sockaddr_in servaddr;
+	int ssock;
+
+	long nthreads;
+	pthread_t *threads;
+
+	while ((opt = getopt(argc, argv, "p:")) != -1) {
+		switch (opt) {
+			case 'p':
+				port = atoi(optarg);
+				break;
+			default: /* '?' */
+				fprintf(stderr, "Usage: %s -p port\n", argv[0]);
+				exit(EXIT_FAILURE);
+		}
+	}
+
+	if (optind < argc) {
+		fprintf(stderr, "Unexpected additional arguments:");
+		for (i = optind; i < argc; i++) {
+			fprintf(stderr, " %s", argv[i]);
+		}
+		fprintf(stderr, "\n");
+		exit(EXIT_FAILURE);
+	}
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	servaddr.sin_port = htons(port);
+
+	ssock = socket(AF_INET, SOCK_STREAM, 0);
+	if (ssock == -1) {
+		// TODO: handle error
+	}
+
+	ret = bind(ssock, (struct sockaddr *)&servaddr, sizeof(servaddr));
+	if (ret == -1) {
+		// TODO: handle error
+	}
+
+	ret = listen(ssock, 0);
+	if (ret == -1) {
+		// TODO: handle error
+	}
+
+	nthreads = 3 * get_nprocs();
+	threads = calloc(nthreads, sizeof(pthread_t));
+
+	for (i = 0; i < nthreads; i++) {
+		ret = pthread_create(&threads[i], NULL, handle_client, &ssock);
+		if (ret != 0) {
+			// TODO: handle error
+		}
+	}
+	for (i = 0; i < nthreads; i++) {
+		ret = pthread_join(threads[i], NULL);
+		if (ret != 0) {
+			// TODO: handle error
+		}
+	}
+
+	close(ssock);
+	return EXIT_SUCCESS;
+}
+
+void * handle_client(void * arg) {
+	int ret;
+	int csock;
+	int ssock = *((int *)arg);
+	struct sockaddr_in client;
+	socklen_t socksize = sizeof(struct sockaddr_in);
+
+	uint32_t challenge;
+
+	while (done == 0) {
+		csock = accept(ssock, (struct sockaddr *)&client, &socksize);
+		if (csock == -1) {
+			// TODO: handle error
+		}
+
+		for (;;) {
+			ret = recvfrom(csock, &challenge , sizeof(challenge), MSG_WAITALL, NULL, NULL);
+			if (ret == -1) {
+				// TODO: handle error
+				break;
+			}
+			if (ret == 0) {
+				// connection closed
+				break;
+			}
+
+			challenge = htonl(ntohl(challenge) + 1);
+
+			ret = sendto(csock, &challenge, sizeof(challenge), 0, NULL, 0);
+			if (ret == -1) {
+				// TODO: handle error
+				break;
+			}
+		}
+
+		close(csock);
+	}
+
+	return NULL;
+}
