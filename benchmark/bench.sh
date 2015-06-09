f847decb420a36a59d442805f37e0d2673b95601
Jon Gjengset <jon@thesquareplanet.com>
2015-06-09 17:00:44 -0400

    Fix benchmark cpu assignment

--- benchmark/bench.sh
+++ benchmark/bench.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-client=$(readlink -f "$DIR/../target/client")
-
-ncores=$(lscpu | grep "^CPU(s):" | sed 's/.* //')
-scores="$1"; shift
-ccores="$1"; shift
-clients="$1"; shift
-((startc=ncores-ccores))
-
-if ((scores+ccores>ncores)); then
-	echo "Cannot use more server+client cores than there are CPU cores" >/dev/stderr
-	exit 1
-fi
-
-((ends=scores-1))
-((endc=ncores-1))
-
 command -v numactl >/dev/null 2>&1
 no_numa=$?
 
@@ -23,6 +6,26 @@ if [ $no_numa -eq 1 ]; then
 	echo "no numactl, so cannot force core locality; exiting..." > /dev/stderr
 	exit 1
 fi
+
+scores="$1"; shift
+ccores="$1"; shift
+clients="$1"; shift
+((needed=scores+ccores));
+
+# where are we?
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+cores=$(numactl -H | grep cpus | sed 's/.*: //' | paste -sd' ')
+cores=($cores)
+ncores=${#cores[@]}
+
+if ((needed>ncores)); then
+	echo "Cannot use more server+client cores ($needed) than there are CPU cores ($ncores)" >/dev/stderr
+	exit 1
+fi
+
+srange=$(echo ${cores[@]} | cut -d' ' -f1-${scores} | tr ' ' ',')
+crange=$(echo ${cores[@]} | rev | cut -d' ' -f1-${ccores} | rev | tr ' ' ',')
 
 args=()
 for i in "$@"; do
@@ -35,14 +38,21 @@ for i in "$@"; do
 	fi
 done
 
-echo numactl -C 0-$ends "${args[@]}" -p 2222 >/dev/stderr
-numactl -C 0-$ends "${args[@]}" -p 2222 &
+# run the server
+echo numactl -C $srange "${args[@]}" -p 2222 >/dev/stderr
+numactl -C $srange "${args[@]}" -p 2222 &
 pid=$!
 
+# let it initialize
 sleep 1
 
-echo numactl -C $startc-$endc "$client" -p 2222 -c $clients >/dev/stderr
-numactl -C $startc-$endc "$client" -p 2222 -c $clients
+# find the client binary
+client=$(readlink -f "$DIR/../target/client")
 
-kill $pid
+# run the client
+echo numactl -C $crange "$client" -p 2222 -c $clients >/dev/stderr
+numactl -C $crange "$client" -p 2222 -c $clients
+
+# terminate the server
+kill $pid 2>/dev/null
 wait $pid 2>/dev/null
