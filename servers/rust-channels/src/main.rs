c7d6d211632961b1824b44fc6f702fa81710d13c
Apostolis Xekoukoulotakis <apostolis.xekoukoulotakis@gmail.com>
2015-06-19 02:26:26 +0300

    Changed the rust-channels server to not require either the ncores or the nclient argument

--- servers/rust-channels/src/main.rs
+++ servers/rust-channels/src/main.rs
@@ -1,5 +1,7 @@
 #![feature(tcp)]
 
+extern crate num_cpus;
+
 use std::sync::mpsc::{Sender, Receiver};
 use std::sync::mpsc;
 use std::net::{TcpListener, TcpStream};
@@ -10,6 +12,7 @@ use std::mem;
 use std::str::FromStr;
 use std::io::Read;
 use std::io::Write;
+
 
 fn main() {
 
@@ -28,39 +31,8 @@ fn main() {
 
     let port = port_.unwrap();
 
-    //Get the number of cores.
-    let ncores_str_ :Option<String> = env::args().skip(4).next();
-
-    if let None = ncores_str_ {
-        println!("usage: {} -p port -c cores", env::args().next().unwrap());
-        return;
-    }
-
-    let ncores_ = u16::from_str(&ncores_str_.unwrap());
-    if let Err(ref e) = ncores_ {
-        println!("invalid cores number given: {}", e);
-        return;
-    }
-
-    let ncores = ncores_.unwrap();
+    let ncores = num_cpus::get() as u16;
     println!("Number of cores: {}", ncores);
-
-    let nclients_str_ :Option<String> = env::args().skip(6).next();
-
-    if let None = nclients_str_ {
-        println!("usage: {} -p port -c cores", env::args().next().unwrap());
-        return;
-    }
-
-    //Get the number of clients.
-    let nclients_ = u16::from_str(&nclients_str_.unwrap());
-    if let Err(ref e) = nclients_ {
-        println!("invalid cores number given: {}", e);
-        return;
-    }
-
-    let nclients = nclients_.unwrap();
-    println!("Number of clients: {}", nclients);
 
 
     //Initialize ncores threads that will process the network traffic.
@@ -69,11 +41,10 @@ fn main() {
     let mut work_tx_vec: Vec<Sender<TcpStream>> = Vec::with_capacity(ncores as usize);
 
     for id in 0..ncores {
-        let (work_tx, work_rx_): (Sender<TcpStream>, Receiver<TcpStream>) = mpsc::channel();
+        let (work_tx, work_rx): (Sender<TcpStream>, Receiver<TcpStream>) = mpsc::channel();
         work_tx_vec.push(work_tx);
 
         {
-            let work_rx = work_rx_; 
             let coord_tx = coord_tx_.clone();
 
             thread::spawn(move || {
@@ -88,9 +59,7 @@ fn main() {
                     if let Err(ref e) = stream {
                         println!("Channel Error: {}", e);
                     }
-                    //        println!("handle client started");
                     handle_client(stream.unwrap());
-                    //        println!("handle client finished");
                 }
             });
         }
@@ -103,50 +72,60 @@ fn main() {
         println!("failed to listen on port: {}", e);
         return;
     }
-
     let listener = listener_.unwrap();
+
     println!("Started listening on port: {}",port);
+
+    let (init_tx_, init_rx): (Sender<TcpStream>, Receiver<TcpStream>) = mpsc::channel();
+
+    //Perform the initialization phase.
+    for _ in 0..ncores  {
+        let init_tx = init_tx_.clone();
+        let listener_clone = listener.try_clone().unwrap();
+
+        thread::spawn(move || {
+            loop {
+                let stream_ = listener_clone.accept();
+                match stream_ {
+                    Ok((mut stream,_)) => {
+                        initialize_connection(&mut stream);
+                        let stream_sent = init_tx.send(stream);
+                        if let Err(ref e) = stream_sent {
+                            println!("Channel Error: {}", e);
+                        }
+
+                    }
+                    Err(e) => {
+                        println!("failed to accept connection: {}", e);
+                    }
+                }
+            }
+        });
+
+    }
 
     //Loop in case we need more statistical data.
     loop {
 
-        //Perform the initialization phase.
-        //Get the streams for each connection.
-        let mut streams: Vec<TcpStream> = Vec::new();
-        for _ in 0..nclients  {
-            let stream_ = listener.accept();
-            match stream_ {
-                Ok((stream,_)) => {
-                    //           println!("Received a connection");
-                    let stream_clone = stream.try_clone().unwrap();
-                    thread::spawn(move || {
-                        initialize_connection(stream_clone);
-                    });
-                    streams.push(stream);
-                }
-                Err(e) => {
-                    println!("failed to accept connection: {}", e);
-                }
-            }
-
-        }
-
         //Perform the network processing by the ncores threads.
         //Each thread notifies the main thread when it can process more clients.
 
-        for stream in streams {
-            let ready = coord_rx.recv();
-            if let Err(ref e) = ready {
-                println!("Channel Error: {}", e);
-            }
-
-            let id = ready.unwrap();
-            let stream_sent = work_tx_vec[id as usize].send(stream);
-            if let Err(ref e) = stream_sent {
-                println!("Channel Error: {}", e);
-            }
-
+        let stream = init_rx.recv();
+        if let Err(ref e) = stream {
+            println!("Channel Error: {}", e);
         }
+
+        let ready = coord_rx.recv();
+        if let Err(ref e) = ready {
+            println!("Channel Error: {}", e);
+        }
+
+        let id = ready.unwrap();
+        let stream_sent = work_tx_vec[id as usize].send(stream.unwrap());
+        if let Err(ref e) = stream_sent {
+            println!("Channel Error: {}", e);
+        }
+
     }
 }
 
@@ -164,7 +143,6 @@ fn handle_client(mut stream: TcpStream) {
             match stream.read(&mut buf[nread..]) {
                 Ok(n) if n == 0 => return,
                 Ok(n) => {nread += n;
-                    //            println!("n:{},nread:{}",n,nread)
                 },
                 Err(ref e) if e.kind() == io::ErrorKind::Interrupted => {}
                 Err(e) => {
@@ -186,7 +164,6 @@ fn handle_client(mut stream: TcpStream) {
         while nwritten < buf.len() {
             match stream.write(&buf[nwritten..]) {
                 Ok(n) => {nwritten += n;
-                    //          println!("nwritten:{}",nwritten);
                 },
                 Err(ref e) if e.kind() == io::ErrorKind::Interrupted => {}
                 Err(e) => {
@@ -200,7 +177,7 @@ fn handle_client(mut stream: TcpStream) {
 
 
 
-fn initialize_connection(mut stream: TcpStream) {
+fn initialize_connection(stream: & mut TcpStream) {
     let mut buf = [0u8; 4];
     let mut challenge;
     let mut nread;
@@ -213,7 +190,6 @@ fn initialize_connection(mut stream: TcpStream) {
         match stream.read(&mut buf[nread..]) {
             Ok(n) if n == 0 => return,
             Ok(n) => {nread += n;
-                //            println!("n:{},nread:{}",n,nread)
             },
             Err(ref e) if e.kind() == io::ErrorKind::Interrupted => {}
             Err(e) => {
@@ -235,7 +211,6 @@ fn initialize_connection(mut stream: TcpStream) {
     while nwritten < buf.len() {
         match stream.write(&buf[nwritten..]) {
             Ok(n) => {nwritten += n;
-                //          println!("nwritten:{}",nwritten);
             },
             Err(ref e) if e.kind() == io::ErrorKind::Interrupted => {}
             Err(e) => {
@@ -244,5 +219,4 @@ fn initialize_connection(mut stream: TcpStream) {
             }
         }
     }
-    //       println!("fininshed initialization");
 }
